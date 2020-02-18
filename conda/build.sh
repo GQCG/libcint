2b2be09f7d13d1ec26b6cd6f2b8866fbd93d2843
guacke <Guillaume.Acke@UGent.be>
2020-02-18 20:13:27 +0100

    Add Conda build recipe.

---
+++ conda/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+mkdir build && cd build
+if [ `uname` == Darwin ]; then
+    ${BUILD_PREFIX}/bin/cmake \
+        .. \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=${CLANG} \
+        -DCMAKE_C_COMPILER=${CLANGXX} \
+        -DCMAKE_C_FLAGS="${CFLAGS} ${OPTS}" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
+        -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+        -DUSE_MKL=TRUE \
+        -DENABLE_TEST=TRUE
+fi
+if [ `uname` == Linux ]; then
+    ${BUILD_PREFIX}/bin/cmake \
+        .. \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=${GCC} \
+        -DCMAKE_C_COMPILER=${GXX} \
+        -DCMAKE_C_FLAGS="${CFLAGS} ${OPTS}" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
+        -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+        -DUSE_MKL=TRUE \
+        -DENABLE_TEST=TRUE
+fi
+
+make -j ${CPU_COUNT}
+make test ARGS=-V
+make install
+
