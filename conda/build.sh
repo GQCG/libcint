63884bbc59e886feb320cff4be87eee732b7e898
guacke <Guillaume.Acke@UGent.be>
2020-02-27 19:00:33 +0100

    Bugfix: cmake cxx compiler.

--- conda/build.sh
+++ conda/build.sh
@@ -8,7 +8,7 @@ if [ `uname` == Darwin ]; then
         -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_C_COMPILER=${CLANG} \
-        -DCMAKE_C_COMPILER=${CLANGXX} \
+        -DCMAKE_CXX_COMPILER=${CLANGXX} \
         -DCMAKE_C_FLAGS="${CFLAGS} ${OPTS}" \
         -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
         -DCMAKE_VERBOSE_MAKEFILE=TRUE \
@@ -21,7 +21,7 @@ if [ `uname` == Linux ]; then
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_C_COMPILER=${GCC} \
-        -DCMAKE_C_COMPILER=${GXX} \
+        -DCMAKE_CXX_COMPILER=${GXX} \
         -DCMAKE_C_FLAGS="${CFLAGS} ${OPTS}" \
         -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
         -DCMAKE_VERBOSE_MAKEFILE=TRUE \
